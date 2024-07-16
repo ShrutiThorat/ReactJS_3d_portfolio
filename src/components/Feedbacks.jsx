9b978db6e42ea0a940e570ada95208b6f94c82ca
ShrutiThorat <75846081+ShrutiThorat@users.noreply.github.com>
2024-07-16 16:28:28 +0530

    Add files via upload

---
+++ src/components/Feedbacks.jsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { SectionWrapper } from '../hoc'
+import { fadeIn, textVariant } from '../utils/motion'
+import { testimonials } from '../constants'
+
+const Feedbacks = () => {
+  return (
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}></p>
+        </motion.div>
+      </div>
+    </div>
+  )
+}
+
+export default Feedbacks
