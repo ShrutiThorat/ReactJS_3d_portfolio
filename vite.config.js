bbae69c58ffe45af57ab1e79b76784f64060b0ab
ShrutiThorat <shrutithorat8816.com>
2024-11-27 18:12:48 +0530

    deploy portfolio to github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ReactJS_3d_portfolio/",
   plugins: [react()],
-})
+});
